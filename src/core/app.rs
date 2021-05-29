cabf62879d082cbce40cf42eb028ffbd53bae8db
ayase <paper.airplane01@gmail.com>
2021-05-29 22:02:47 +0900

    スペースを追加しました。

--- src/core/app.rs
+++ src/core/app.rs
@@ -4,15 +4,19 @@ use imgui_glfw_rs::imgui;
 #[allow(unused_variables)]
 pub trait App {
     fn new(ctx: &Context) -> Self;
+
     fn update(&mut self, ctx: &Context) -> Result<(), String> {
         Ok(())
     }
+
     fn render(&self, ctx: &Context) -> Result<(), String> {
         Ok(())
     }
+
     fn ui_overlay<'a>(&self, ui: imgui::Ui<'a>) -> imgui::Ui<'a> {
         ui
     }
+
     fn destroy(&mut self, ctx: &Context) -> Result<(), String> {
         Ok(())
     }
